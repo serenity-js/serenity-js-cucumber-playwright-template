5208f0883e7d3b021e1a65b53b9b7c321c05c4a7
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2025-06-20 16:39:49 +0100

    fix(serenity-js): updated to Serenity/JS 3.32

--- test/Actors.ts
+++ test/Actors.ts
@@ -1,19 +1,20 @@
 import { Actor, Cast, TakeNotes } from '@serenity-js/core';
-import { BrowseTheWebWithPlaywright, PlaywrightOptions } from '@serenity-js/playwright';
+import { BrowseTheWebWithPlaywright, ExtraBrowserContextOptions } from '@serenity-js/playwright';
 import { CallAnApi } from '@serenity-js/rest';
 import * as playwright from 'playwright';
 
 export class Actors implements Cast {
     constructor(
         private readonly browser: playwright.Browser,
-        private readonly options: PlaywrightOptions,
+        private readonly browserContextOptions: playwright.BrowserContextOptions,
+        private readonly extraBrowserContextOptions: ExtraBrowserContextOptions,
     ) {
     }
 
     prepare(actor: Actor): Actor {
         return actor.whoCan(
-            BrowseTheWebWithPlaywright.using(this.browser, this.options),
-            CallAnApi.at(this.options.baseURL),
+            BrowseTheWebWithPlaywright.using(this.browser, this.browserContextOptions, this.extraBrowserContextOptions),
+            CallAnApi.at(this.browserContextOptions.baseURL),
             TakeNotes.usingAnEmptyNotepad(),
         );
     }
